f4ea3d365888e3f15ef0389ce7f65cad622684fe
= <=>
2024-07-26 22:41:59 -0700

    cockroach

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let audio1 = new Audio('mp3s/world_domination_1.mp3');
+    let audio2 = new Audio('mp3s/world_domination_2.mp3');
+    let audio3 = new Audio('mp3s/world_domination_3.mp3');
+    let audio4 = new Audio('mp3s/world_domination_4.mp3');
 
-let audio1 = new Audio('mp3s/world_domination_1.mp3');
-let audio2 = new Audio('mp3s/world_domination_2.mp3');
-let audio3 = new Audio('mp3s/world_domination_3.mp3');
-let audio4 = new Audio('mp3s/world_domination_4.mp3');
+    let index = 0;
+    const reel = [audio1, audio2, audio3, audio4];
 
-let index = 0;
+    const cockroachHandler = () => {
+        try {
+            reel[index].play();
+            console.log('click');
+            index++;
+        } catch (error) {
+            console.error(error)
+        }
+    }
 
-let clips = [audio1, audio2, audio3, audio4];
 
-const cockroachHandler = () => {
-    clips[index].play();
-    index++
-}
 
-const button = document.getElementById("button");
-
-button.addEventListener('click', cockroachHandler);
+    const button = document.querySelector(".button");
+        button.addEventListener('click', cockroachHandler);
+});
